261d8ea625d1df0136fef56cab80370ea83e3a77
lllllaura <60121024+lllllaura@users.noreply.github.com>
2020-10-03 09:41:09 +0800

    Update LSTM.py

--- model/LSTM.py
+++ model/LSTM.py
@@ -3,13 +3,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 class Model(nn.Module) :
-    def __init__(self, hidden_dim = 18, input_dim = 14 * 5 * 50, num_layers = 5, batch_size = 30) :
+    def __init__(self, hidden_dim = 18, input_dim = 14 * 5 , num_layers = 5, batch_size = 30) :
         self.batch_size = batch_size
         self.num_layers = num_layers
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         
-        super(LSTM, self).__init__()
+        super(Model, self).__init__()
         self.lstm = nn.LSTM(
             input_size = input_dim,
             hidden_size = hidden_dim,
